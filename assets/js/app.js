62896f712ca9678d43ff428ac976c61a7b7231bc
JeremyARussell <jeremyarussell@gmail.com>
2014-08-24 01:24:10 -0700

    Finished getting the existing design incorporated into the sails/Angular setup
    Some Grunt/SASS situation to resolve, mentioned in the readme.md, some
    unneeded lines of code in the main.scss, edited grunt configurations to
    support SASS.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,12 +2,12 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+angular.module('ISPCU', [
   'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+  'ISPCU.filters',
+  'ISPCU.services',
+  'ISPCU.directives',
+  'ISPCU.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -2,7 +2,7 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
+angular.module('ISPCU.controllers', []).
   controller('MyCtrl1', [function() {
 
   }])
